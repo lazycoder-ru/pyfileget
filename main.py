b29f5499a5842746cff0d484317f6b4ea62232e3
Dmitriy <dem.mort@gmail.com>
2014-02-07 19:02:49 +0400

    add test suit

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Just for test file download module
+
+from filedownload import downloadfile
+
+def main():
+	
+	
+	return 0
+
+if __name__ == '__main__':
+	main()
+
