beef81714f859fcf89a1cf9b2c3edd0ceb77dcbd
Dmitriy <dem.mort@gmail.com>
2014-02-08 03:27:21 +0400

    main.py is default

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from filedownload import downloadfile
 
 def main():
 	home = os.path.expanduser("~")
-	downloadfile("http://mirror.yandex.ru/ubuntu-releases/13.10/ubuntu-13.10-desktop-i386.iso", newName="pyfiledownload", folderpath=home)
+	downloadfile("http://mirror.yandex.ru/ubuntu-releases/13.10/ubuntu-13.10-desktop-i386.iso", folderpath=home)
 	return 0
 
 if __name__ == '__main__':
