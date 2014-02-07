d001d438f366ceed640dbef00b5008bb1c7c18c4
Dmitriy <dem.mort@gmail.com>
2014-02-07 18:57:48 +0400

    add speedmeter

--- filedownload.py
+++ filedownload.py
@@ -9,9 +9,9 @@ def getConsoleWeight():
         columns = os.popen('stty size', 'r').read().split()[1]
     return int(columns)
     
-def getLoadingBar(ln, percent=100, bracket="[]", fillch="#", emptych="-"):
+def getLoadingBar(ln, procent=100, bracket="[]", fillch="#", emptych="-"):
     sumln = ln-2 #2 brackets
-    filled = int(percent*sumln/100)
+    filled = int(procent*sumln/100)
     bar = "%s%s%s%s" % (bracket[0], fillch*filled, emptych*(sumln-filled), bracket[1])
     return bar
 
@@ -31,6 +31,7 @@ def getNewPath(url, name=None, directory=None):
     return path
 
 def downloadfile(url, newName=None, dir=None):
+    cols = getConsoleWeight()
     newPath = getNewPath(url, newName, dir)
     
     if not os.path.exists(newPath):
@@ -64,11 +65,9 @@ def downloadfile(url, newName=None, dir=None):
         
     if localLen == 0:
         localFile = open(newPath, "wb")
-        isNewDownload = True
     elif localLen < remoteLen:
         localFile = open(newPath, "ab")
-        isNewDownload = False
-    elif localLen >= remoteLen:
+    elif localLen == remoteLen:
         print "File has downloaded already.\n"
         return
 
@@ -80,25 +79,31 @@ def downloadfile(url, newName=None, dir=None):
     except urllib2.URLError, e:
         print e
         return
-        
-    cols = getConsoleWeight()
-    if isNewDownload: print "Downloading: %s" % (remoteFile.url)
-    else: print "Continue downloading: %s" % (remoteFile.url)
-    if remoteLen == 0: raise urllib2.HTTPError("Content-Length is 0")
-    remoteLen = float(remoteLen)
-    bytesRead = float(localLen)
-    for line in remoteFile:
-        bytesRead += len(line)
-        percent = 100*bytesRead/remoteLen
-        curInfo = "\rProgress: %.02f/%.02f kb (%d%%)" % (
-            bytesRead/1024.0,
-            remoteLen/1024.0,
-            percent)
-        loadbar = getLoadingBar(cols-len(curInfo), percent)
-        curInfo = curInfo[:11] + loadbar + curInfo[10:]
-        sys.stdout.write(curInfo)
-        sys.stdout.flush()
-        localFile.write(line)
+    
+    print "Downloading %s - %s (%s bytes) ..." % (remoteFile.url, newPath, remoteLen)
+    if remoteLen != 0:
+        remoteLen = float(remoteLen)
+        bytesRead = float(localLen)
+        t = time.time()
+        rm = bytesRead
+        speed = 0.0
+        for line in remoteFile:
+            if time.time()-t >= 1.0:
+                t = time.time()
+                speed = (bytesRead-rm)/1024.0
+                rm = bytesRead
+            bytesRead += len(line)
+            percent = 100*bytesRead/remoteLen
+            curInfo = "\rProgress: %.02f/%.02f kb (%d%%) %.02f kb/s" % (
+                bytesRead/1024.0,
+                remoteLen/1024.0,
+                percent,
+                speed)
+            loadbar = getLoadingBar(cols-len(curInfo), percent)
+            curInfo = curInfo[:11] + loadbar + curInfo[10:]
+            sys.stdout.write(curInfo)
+            sys.stdout.flush()
+            localFile.write(line)
     remoteFile.close()
     localFile.close()
     print "\nFile [%s] has been downloaded.\n" % newPath
