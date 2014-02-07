93079142f0297096d6bb30af5a65c2a6dcfe2963
Dmitriy <dem.mort@gmail.com>
2014-02-07 22:12:38 +0400

    Some minor changes

--- filedownload.py
+++ filedownload.py
@@ -12,33 +12,23 @@ def getConsoleWeight():
 def getLoadingBar(ln, procent=100, bracket="[]", fillch="#", emptych="-"):
     sumln = ln-2 #2 brackets
     filled = int(procent*sumln/100)
-    bar = "%s%s%s%s" % (bracket[0], fillch*filled, emptych*(sumln-filled), bracket[1])
-    return bar
+    return "%s%s%s%s" % (bracket[0], fillch*filled, emptych*(sumln-filled), bracket[1])
 
 def getNewPath(url, name=None, folderpath=None):
     if folderpath==None: folderpath=os.curdir
     else: folderpath = folderpath.rstrip(os.sep)
     if not os.path.exists(folderpath): os.makedirs(folderpath)
     if name==None:
-        path="%s%s%s" % (
-            folderpath,
-            os.sep,
-            url.split("/")[-1])
+        path="%s%s%s" % (folderpath, os.sep, url.split("/")[-1])
     else:
-        path="%s%s%s" % (
-            folderpath,
-            os.sep,
-            name)
+        path="%s%s%s" % (folderpath, os.sep, name)
     return path
 
 def downloadfile(url, newName=None, folderpath=None):
-    cols = getConsoleWeight()
     newPath = getNewPath(url, newName, folderpath)
     
-    if not os.path.exists(newPath):
-        localLen = 0
-    else:
-        localLen = os.path.getsize(newPath)
+    if not os.path.exists(newPath): localLen = 0
+    else: localLen = os.path.getsize(newPath)
         
     req = urllib2.Request(url)
     req.headers['Range'] = 'bytes='+str(localLen)+'-'
@@ -80,31 +70,35 @@ def downloadfile(url, newName=None, folderpath=None):
     except urllib2.URLError, e:
         print e
         return
-    
-    print "Downloading %s - %s (%s bytes) ..." % (remoteFile.url, newPath, remoteLen)
-    if remoteLen != 0:
-        remoteLen = float(remoteLen)
-        bytesRead = float(localLen)
-        t = time.time()
-        rm = bytesRead
-        speed = 0.0
-        for line in remoteFile:
-            bytesRead += len(line)
-            if time.time()-t >= 1.0:
-                speed = (bytesRead-rm)/1024.0
-                rm = bytesRead
-                t = time.time()
-            percent = 100*bytesRead/remoteLen
-            curInfo = "\rProgress: %.02f/%.02f kb (%d%%) %.02f kb/s" % (
-                bytesRead/1024.0,
-                remoteLen/1024.0,
-                percent,
-                speed)
-            loadbar = getLoadingBar(cols-len(curInfo), percent)
-            curInfo = curInfo[:11] + loadbar + curInfo[10:]
-            sys.stdout.write(curInfo)
-            sys.stdout.flush()
-            localFile.write(line)
+        
+    cols = getConsoleWeight()
+    print "Downloading: %s" % remoteFile.url
+    #maybe not raise and just return
+    if remoteLen == 0: raise urllib2.HTTPError("Content-Length is 0")
+    remoteLen = float(remoteLen)
+    bytesRead = float(localLen)
+    t = time.time()
+    rm = bytesRead
+    speed = 0.0
+    for line in remoteFile:
+        bytesRead += len(line)
+        if time.time()-t >= 1.0:
+            speed = (bytesRead-rm)/1024.0
+            rm = bytesRead
+            t = time.time()
+        percent = 100*bytesRead/remoteLen
+        #TODO: make templated output
+        curInfo = "\rProgress: %.02f/%.02f KB (%d%%) %.02f KB/s" % (
+            bytesRead/1024.0,
+            remoteLen/1024.0,
+            percent,
+            speed)
+        loadbar = getLoadingBar(cols-len(curInfo), percent)
+        curInfo = curInfo[:11] + loadbar + curInfo[10:] #after progress
+        sys.stdout.write(curInfo)
+        sys.stdout.flush()
+        localFile.write(line)
     remoteFile.close()
     localFile.close()
     print "\nFile [%s] has been downloaded.\n" % newPath
+    return
